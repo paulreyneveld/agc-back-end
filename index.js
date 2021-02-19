fdfa2846caf7d055b82a5809d4e8c7f4150237fe
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-18 18:36:34 -0800

    Added ability to add users to the back end via post man

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const usersRouter = require('./controllers/users')
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
 const password = process.env.DB_PASSWORD
 const url =
-  `mongodb+srv://dbUser:${password}@agc.nbvbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  `mongodb+srv://dbUser:${password}@agc.nbvbb.mongodb.net/agcdb?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => console.log('Conected to DB'))
   .catch(error => console.log('Failed to connect', error.message))
+
+app.use(express.json())
 
 app.use('/api/users', usersRouter)
 
