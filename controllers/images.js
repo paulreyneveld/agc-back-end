e7ab63b225276d2fd716ab460451edeadd833c43
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-03-16 18:03:23 -0700

    Added an images router

---
+++ controllers/images.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const multer = require('multer');
+const Image = require('../models/Image');
+const imagesRouter = express.Router();
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000 // max file size 1MB = 1000000 bytes
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpeg|jpg)$/)) {
+      cb(new Error('only upload files with jpg or jpeg format.'));
+    }
+    cb(undefined, true); // continue with upload
+  }
+});
+
+imagesRouter.post(
+  '/',
+  upload.single('image'),
+  async (req, res) => {
+    try {
+      const image = new Image(req.body);
+      const file = req.file.buffer;
+      image.image = file;
+
+      await image.save();
+
+      res.status(201).send({ _id: image._id });
+    } catch (error) {
+      res.status(500).send({
+        upload_error: 'Error while uploading file...Try again later.'
+      });
+    }
+  },
+  (error, req, res, next) => {
+    if (error) {
+      res.status(500).send({
+        upload_error: error.message
+      });
+    }
+  }
+);
+
+imagesRouter.get('/', async (req, res) => {
+  try {
+    const images = await Image.find({});
+    res.send(images);
+  } catch (error) {
+    res.status(500).send({ get_error: 'Error while getting list of images.' });
+  }
+});
+
+imagesRouter.get('/:id', async (req, res) => {
+  try {
+    const result = await Image.findById(req.params.id);
+    res.set('Content-Type', 'image/jpeg');
+    res.send(result.image);
+  } catch (error) {
+    res.status(400).send({ get_error: 'Error while getting image.' });
+  }
+});
+
+module.exports = imagesRouter;
