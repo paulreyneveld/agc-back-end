93bc5a09e57ff10bd5f9bc7cceca5c447dc6e648
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-04-11 20:58:37 -0700

    Added middleware for JWT auth on the back end

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,17 @@ const express = require('express')
 const blogRouter = express.Router()
 const Blog = require('../models/Blog')
 
+const authenticateMiddleware = (req, res, next) => {
+    const { authorization } = req.headers
+    const token = authorization && authorization.split(" ")[1]
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
+     })
+}
+
 blogRouter.post('/', async (req, res) => {
     try {
         const body = req.body
