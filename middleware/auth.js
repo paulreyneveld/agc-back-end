887e09d140428df424dca1211c09d60732c33809
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-04-12 23:26:47 -0700

    Committing a file for authentication

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+const authenticateMiddleware = (req, res, next) => {
+    const SECRET_KEY = process.env.SECRET
+    const { authorization } = req.headers
+    const token = authorization && authorization.split(" ")[1]
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
+     })
+}
